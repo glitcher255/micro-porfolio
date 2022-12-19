1e195160cdb032c5149bf98ae4eda25ae2cde5cd
Your Name <you@example.com>
2022-12-20 00:39:52 +0200

    Added mobile drop down branch - fixes

--- script.js
+++ script.js
@@ -20,8 +20,10 @@ document.getElementsByClassName('hamburger_button')[0].addEventListener('click',
 })
 
 function close_button() {
+    if (screen.width < 768) {
     document.getElementById('item_nav').style.display = 'none'
     document.getElementById('header').style.display = 'none'
     document.getElementsByTagName('body')[0].style.overflow = 'scroll'
     is_on = false
+    }
 }
