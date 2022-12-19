a70c9d9a7757f10a996713720e99efa02b287521
Your Name <you@example.com>
2022-12-19 21:58:01 +0200

    Added dropdown menu for mobile

---
+++ script.js
@@ -0,0 +1,27 @@
+let is_on = Boolean
+
+document.getElementsByClassName('hamburger_button')[0].addEventListener('click', function () {
+   
+    if (is_on === true) {
+        document.getElementById('item_nav').style.display = 'none'
+        document.getElementById('header').style.display = 'none'
+        document.getElementsByTagName('body')[0].style.overflow = 'scroll'
+        is_on = false
+        console.log (is_on)
+    }
+
+    else {
+    document.getElementById('item_nav').style.display = 'flex'
+    document.getElementById('header').style.display = 'flex'
+    document.getElementsByTagName('body')[0].style.overflow = 'hidden'
+    is_on = true
+    console.log (is_on)
+    }
+})
+
+function close_button() {
+    document.getElementById('item_nav').style.display = 'none'
+    document.getElementById('header').style.display = 'none'
+    document.getElementsByTagName('body')[0].style.overflow = 'scroll'
+    is_on = false
+}
