9be227676a909d22a36aa1a7d444a1288e7108c6
Your Name <you@example.com>
2022-12-20 17:47:16 +0200

    Changed styling, added x to menu drop downs

--- script.js
+++ script.js
@@ -5,6 +5,9 @@ document.getElementsByClassName('hamburger_button')[0].addEventListener('click',
     if (is_on === true) {
         document.getElementById('item_nav').style.display = 'none'
         document.getElementById('header').style.display = 'none'
+        document.getElementsByClassName('hamburger_button')[0].background = "#3c3a39"
+        document.getElementById('hamburger_button_x').style.display = "none"
+        
         document.getElementsByTagName('body')[0].style.overflow = 'scroll'
         is_on = false
         console.log (is_on)
@@ -13,6 +16,8 @@ document.getElementsByClassName('hamburger_button')[0].addEventListener('click',
     else {
     document.getElementById('item_nav').style.display = 'flex'
     document.getElementById('header').style.display = 'flex'
+    document.getElementsByClassName('hamburger_button')[0].background = "none"
+    document.getElementById('hamburger_button_x').style.display = "block"
     document.getElementsByTagName('body')[0].style.overflow = 'hidden'
     is_on = true
     console.log (is_on)
@@ -23,6 +28,7 @@ function close_button() {
     if (screen.width < 768) {
     document.getElementById('item_nav').style.display = 'none'
     document.getElementById('header').style.display = 'none'
+    document.getElementById('hamburger_button_x').style.display = "none"
     document.getElementsByTagName('body')[0].style.overflow = 'scroll'
     is_on = false
     }
